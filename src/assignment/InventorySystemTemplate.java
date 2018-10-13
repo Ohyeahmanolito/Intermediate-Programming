3cf454b70dd3bdfe30ffb1d0478cfdae96fc38aa
Mano Octaviano <manolito.octaviano@yahoo.com>
2018-10-13 13:30:18 +0800

    Basic inventory management system template.

---
+++ src/assignment/InventorySystemTemplate.java
@@ -0,0 +1,60 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package assignment;
+
+/**
+ *
+ * @author mvoctavianojr
+ */
+public class InventorySystemTemplate {
+
+    /**
+     * This method should return the total count of products in inventory.
+     *
+     * @return total number of products.
+     */
+    int getTotalProductsCount() {
+        return 0;
+    }
+
+    /**
+     * This method should return the count of the product in inventory.
+     *
+     * @param item product you want to get the total count.
+     */
+    int getProductCount(String item) {
+        return 0;
+    }
+
+    /**
+     * This method should be able to add new product in the inventory.
+     *
+     * @param item product name to be added.
+     * @param count number of items in the inventory.
+     */
+    void addNewProduct(String item, int count) {
+    }
+
+    /**
+     * This method should be able to edit the product count in the inventory.
+     *
+     * @param item product name that will be edited.
+     */
+    void updateProductCount(String item) {
+    }
+    
+    /**
+     * This method should be able to delete product in the inventory.
+     *
+     * @param item product name that will be deleted.
+     *
+     * @return the product and its corresponding count in the inventory.
+     */
+    String deleteProduct(String item) {
+        return "";
+    }
+
+}
